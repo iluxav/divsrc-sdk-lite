7f6fbd0a22ef90cafa40a1a88ef8c423c737ebef
Ilya Vinokurov <ilya@supersonic.com>
2021-08-19 17:56:36 +0300

    Fixes

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,59 +1,60 @@
-
 export interface ArtifactResponse {
-  installations: Artifact[]
-  zones: string[],
+  installations: Artifact[];
+  zones: string[];
   importmaps: {
-    [key: string]: string
-  }
+    [key: string]: string;
+  };
 }
-
 
 export type IdentityFields = {
   [key: string]: any;
-}
+};
 
 export interface Artifact {
-  installationId: string
-  baseUrl?: string
-  artifactId: string
-  zone: string
-  fileName?: string
-  hashJWT?: string
-  version: string
-  mappingRules?: any
+  installationId: string;
+  baseUrl?: string;
+  artifactId: string;
+  zone: string;
+  fileName?: string;
+  hashJWT?: string;
+  version: string;
+  mappingRules?: any;
+  url?: string;
 }
 
 export interface Emulator {
-  url: string
-  components: {
-    [key: string]: boolean | object
-  } | string[]
+  url: string;
+  components:
+    | {
+        [key: string]: boolean | object;
+      }
+    | string[];
 }
 
 export interface Config {
-  key?: string,
-  identify?: any,
+  key?: string;
+  identify?: any;
   importmaps?: {
-    [key: string]: string
-  }
-  installationMapUrl?: string
-  es6ModuleLoader?: boolean | undefined,
-  emulators?: Emulator[],
-  systemJsUrl?: string,
-  systemJsExtrasAmd?: string,
-  systemJsExtrasNamed?: string,
-  injectSystemJs?: boolean,
-  injectImportmap?: boolean,
-  cdnMode?: boolean
+    [key: string]: string;
+  };
+  installationMapUrl?: string;
+  es6ModuleLoader?: boolean | undefined;
+  emulators?: Emulator[];
+  systemJsUrl?: string;
+  systemJsExtrasAmd?: string;
+  systemJsExtrasNamed?: string;
+  injectSystemJs?: boolean;
+  injectImportmap?: boolean;
+  cdnMode?: boolean;
 }
 
 export interface IDivSrcCore {
-  import(moduleName: string): Promise<any>
-  generateArtifactBaseUrl(artifact: Artifact): string
-  generateArtifactUrl(moduleName: Artifact): string
-  overrideArtifactByInstallationId(artifact: Artifact)
-  overrideArtifactById(artifact: Artifact)
-  getArtifact(zone: string): Artifact | undefined
-  identify(fields: IdentityFields)
-  onIdentityChange(callback: void)
+  import(moduleName: string): Promise<any>;
+  generateArtifactBaseUrl(artifact: Artifact): string;
+  generateArtifactUrl(moduleName: Artifact): string;
+  overrideArtifactByInstallationId(artifact: Artifact);
+  overrideArtifactById(artifact: Artifact);
+  getArtifact(zone: string): Artifact | undefined;
+  identify(fields: IdentityFields);
+  onIdentityChange(callback: void);
 }
