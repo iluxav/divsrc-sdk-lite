091bfd4e684ae237f516920f2af582119e7af3ab
Ilya Vinokurov <iluxa.v@gmail.com>
2021-09-09 21:57:39 +0300

    fixes

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {
   IDivSrcCore,
   IdentityFields,
 } from "./interfaces";
-import {loadWebpackModuleComponent} from './WebpackFederation'
+
 
 declare var __webpack_init_sharing__: any;
 declare var __webpack_share_scopes__: any;
